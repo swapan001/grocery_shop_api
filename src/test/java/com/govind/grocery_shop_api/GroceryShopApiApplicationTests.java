7a900b7f511f214aef2b069ca227d78f7285ab96
swapan001 <swan03228@gmail.com>
2026-04-09 14:59:29 +0530

    congigure and setup

---
+++ src/test/java/com/govind/grocery_shop_api/GroceryShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govind.grocery_shop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
