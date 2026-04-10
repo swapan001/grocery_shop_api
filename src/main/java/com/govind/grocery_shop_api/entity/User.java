cf96f6ccbd192429027e2813591c7cac3584f14e
swapan001 <swan03228@gmail.com>
2026-04-10 23:42:24 +0530

    customUserDetails for security

--- src/main/java/com/govind/grocery_shop_api/entity/User.java
+++ src/main/java/com/govind/grocery_shop_api/entity/User.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/govind/grocery_shop_api/service/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.govind.grocery_shop_api.service.security;
+
+import com.govind.grocery_shop_api.entity.User;
+import com.govind.grocery_shop_api.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepo userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+
+}
