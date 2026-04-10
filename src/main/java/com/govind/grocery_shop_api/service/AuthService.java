9408d373c2b51f172d8428ab6e2272f3824bc886
swapan001 <swan03228@gmail.com>
2026-04-11 01:07:14 +0530

    AuthService done

--- src/main/java/com/govind/grocery_shop_api/service/AuthService.java
+++ src/main/java/com/govind/grocery_shop_api/service/AuthService.java
@@ -1,2 +1,39 @@
-package com.govind.grocery_shop_api.service;public class AuthService {
+package com.govind.grocery_shop_api.service;
+
+import com.govind.grocery_shop_api.dto.AuthRequest;
+import com.govind.grocery_shop_api.entity.User;
+import com.govind.grocery_shop_api.repo.UserRepo;
+import com.govind.grocery_shop_api.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    public void register(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("USER");
+        userRepo.save(user);
+    }
+
+    public String login(AuthRequest request) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        return jwtUtil.generateToken(request.getEmail());
+    }
 }
